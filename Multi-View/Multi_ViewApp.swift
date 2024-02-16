b67b93243fdcd4a88dea01702b984cbd381c78b0
Muhammad Naufal Adli <naufaladli0406@gmail.com>
2024-02-16 14:29:11 +0700

    Initial Commit

---
+++ Multi-View/Multi_ViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Multi_ViewApp.swift
+//  Multi-View
+//
+//  Created by Naufal Adli on 16/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Multi_ViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
